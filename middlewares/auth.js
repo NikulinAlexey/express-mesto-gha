39375943ba1bec3a85cb17c14e2dd94840fb1a90
Aleksey Nikulin <donaldduckisnotdead@gmail.com>
2023-07-20 21:15:07 +0300

    refactor: Скорректировал несколько ошибок после 2 ревью

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,7 +8,7 @@ const auth = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'SECRET');
   } catch (err) {
-    next(new UnauthorizedError('Ошибка авторизации'));
+    return Promise.reject(new UnauthorizedError('Ошибка авторизации'));
   }
 
   req.user = payload;
--- middlewares/error.js
+++ middlewares/error.js
@@ -1,10 +1,26 @@
+const NotFoundError = require('../errors/not-found-error');
+const ValidationError = require('../errors/validation-error');
+const ForbiddenError = require('../errors/forbidden-error');
+const UnauthorizedError = require('../errors/unauthorized-error');
+
 const errorHandler = (err, req, res, next) => {
   if (err.code === 11000) {
     res.status(409).send({ message: 'Такой email уже есть в базе' });
-  } else if (err instanceof Error) {
-    res.status(err.statusCode).send({ message: err.message });
-  } else {
-    res.status(500).send({ message: 'На сервере произошла ошибка' });
+  } else if (err.statusCode === 400 || err.statusCode === 401 || err.statusCode === 403) {
+    if (err instanceof UnauthorizedError) {
+      // statusCode(401)
+      res.status(err.statusCode).send({ message: err.message });
+    } else if (err instanceof NotFoundError) {
+      res.status(err.statusCode).send({ message: err.message });
+    } else if (err instanceof ValidationError) {
+      // statusCode(400)
+      res.status(err.statusCode).send({ message: err.message });
+    } else if (err instanceof ForbiddenError) {
+      // statusCode(403)
+      res.status(err.statusCode).send({ message: err.message });
+    } else {
+      res.status(500).send({ message: 'На сервере произошла ошибка' });
+    }
   }
   next();
 };
